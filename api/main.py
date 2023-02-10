da9b4146cb80a9291a41b916e5d091e54af60e3c
patrycjaweiss <59768082+patrycjaweiss@users.noreply.github.com>
2023-02-10 12:49:27 +0100

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from pydantic import BaseModel, ValidationError, validator
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+
+from .ml.model import Model, get_model, n_features
+
+
+class PredictRequest(BaseModel):
+    data: List[List[float]]
+
+    @validator("data")
+    def check_dimensionality(cls, v):
+        for point in v:
+            if len(point) != n_features:
+                raise ValueError(f"Each data point must contain {n_features} features")
+
+        return v
+
+
+class PredictResponse(BaseModel):
+    data: List[float]
+
+
+app = FastAPI()
+
+
+@app.post("/predict", response_model=PredictResponse)
+def predict(input: PredictRequest, model: Model = Depends(get_model)):
+    X = np.array(input.data)
+    y_pred = model.predict(X)
+    result = PredictResponse(data=y_pred.tolist())
+
+    return result
